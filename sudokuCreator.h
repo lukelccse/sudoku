c2a92ceceede5b041e0eb2094326f604d0e1c63c
LukeCSE <z5209728@ad.unsw.edu.au>
2023-07-24 12:26:21 +1000

    changed copy constructor in sudokuPuzzle class

---
+++ sudokuCreator.h
@@ -0,0 +1,7 @@
+/********************
+ * AUTHOR: lukelccse
+ * DATE CREATED: 23.07.23
+ * DATE MODIFIED: 23.07.23
+ * FILE: sudokuCreator.cpp
+ * DESC: contains the sudokuCreator header
+ ********************/
